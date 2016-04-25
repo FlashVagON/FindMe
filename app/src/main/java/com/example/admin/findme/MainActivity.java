a8221c76dc8d57b28ba59504ee8daa6667c413b3
FlashVagON <djvagon@mail.ru>
2016-04-25 21:02:33 +0500

    Create MainActivity class and layout

---
+++ app/src/main/java/com/example/admin/findme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.admin.findme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 25.04.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
